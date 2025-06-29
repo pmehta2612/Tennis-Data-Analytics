bbc0751ecbc0fa7db513ee8d1cd95c934bc16864
Prachi Mehta <mehtaprachi2501@gmail.com>
2025-06-29 19:49:16 +0530

    Delete Streamlit/utils.py

--- Streamlit/utils.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from db_config import get_connection
-
-def run_query(query, params=None):
-    conn = get_connection()
-    df = pd.read_sql(query, conn, params=params)
-    conn.close()
-    return df
